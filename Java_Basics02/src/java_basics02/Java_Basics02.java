15908600f862189bf5f88e8c279706eec105f9e1
DonAlahakoon <rashithaalahakoon@gmail.com>
2022-08-16 12:21:13 +0530

    adding Java_Basics02 file

---
+++ Java_Basics02/src/java_basics02/Java_Basics02.java
@@ -0,0 +1,121 @@
+
+package java_basics02;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class Java_Basics02 {
+
+
+    public static void main(String[] args) {
+       
+        //User Input
+        
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String input = in.nextLine();  //.nextDouble(),.nextInt();
+        System.out.println("Hello "+input);
+        System.out.println(in.hasNextLine());//.hasNextInt();//checks whether there is a input if so return true
+        System.out.println("\n\n");
+
+        //Arrays
+        
+        int luckyNumbers[] = {1,5,8,12,53,35};
+        System.out.println(luckyNumbers[0]);
+        System.out.println(luckyNumbers.length);
+        System.out.println("\n\n");
+        
+       //N dimesional arrays
+        int numberGrid[][]  = {{1,3},{3,4}};
+        numberGrid[0][1] = 99;
+        System.out.println(numberGrid[0][0]);
+        System.out.println(numberGrid[0][1]);
+        System.out.println("\n\n");
+        
+        //ArrayList
+        
+        ArrayList<String> friends = new ArrayList<String>();
+        friends.add("Oscar");
+        friends.add("Angela");
+        friends.add("Kevin");
+        
+        friends.remove("Oscar");
+        System.out.println(friends.toString());
+        System.out.println(friends.get(0));
+        System.out.println(friends.contains("Oscar"));
+        System.out.println(friends.size());
+        System.out.println("\n\n");
+        
+        //Methods
+        
+        int sum = addNumbers(4,60);  // addNumbers method is at the end of the code
+        System.out.println(sum);
+        
+        //IF statements 
+        boolean isStudent = false;
+        boolean isSmart  = false;
+        
+        if(isStudent&&isSmart){
+            System.out.println("You are smart student");
+        }
+        else if(isStudent&&!isSmart){
+            System.out.println("You are studetn not smart");
+        }
+        else{
+            System.out.println("You are not a student nor smart");
+        }
+        
+        if(1<3){
+            System.out.println("Number comparison was true");
+        }
+        if('a'<'b'){
+            System.out.println("Charater comaparison was true");
+        }
+        if("dog".equals("dog")){
+            System.out.println("String comparison was true");
+            System.out.println("\n\n");
+        }
+        
+        //Switch statement
+        char myGrade = 'F';
+        switch(myGrade){
+            case 'A':System.out.println("You passed.");break;
+            case 'F': System.out.println("You fail");break;
+            
+            default:System.out.println("Invalid grade");
+        }
+        
+        //while loops
+        int index = 1;
+        while(index <=5){
+            System.out.println(index);
+            index++;
+        }System.out.println("\n\n");
+        
+        do{
+            System.out.println(index);
+             index++;
+          }while(index<=5);System.out.println("\n\n");
+
+        //For loop
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+        }
+        System.out.println("\n\n");
+        
+        for(int num :luckyNumbers){
+               System.out.print(num+",");
+         }
+        
+        
+        
+    }
+    //static method (which is belong to class rather than the object
+    public static int addNumbers(int num1,int num2){
+        return num1+num2;
+    }
+    
+    
+    
+}
